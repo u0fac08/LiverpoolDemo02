e199d9a81cfdaa43dcb7b9654400d906a707706d
Mark garcia <msgarcia@us.ibm.com>
2025-01-28 13:23:13 -0500

    Added LoginSV100

---
+++ LiverpoolDemo/Logical/VirtualServices.scm/config.scm
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<editableResource createdTimestamp="1738088472573" createdUser="Mark"
+    id="5cb6a4e4:194ade8de35:-7d22" type="service_component_resource"
+    updatedTimestamp="1738088472573" updatedUser="Mark" version="1.1">
+    <loggingConfiguration debug="false" resultWriterLoggingLevel="FULL" writeStubEventsToDB="false"/>
+</editableResource>
